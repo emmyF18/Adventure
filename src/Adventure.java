c7985e8327dfe16d7e6e2c1f2eefba13dae5afa0
emmyF18 <emmyfarrell18@gmail.com>
2017-06-08 17:29:47 -0500

    worked on battle system, leveling, and a few small tweaks

--- src/Adventure.java
+++ src/Adventure.java
@@ -2,34 +2,38 @@ import java.util.Scanner;
 
 public class Adventure 
 {
-	static int level = 1;
-	static int  exp;
-	static int expNeeded = 40 ;
-	static int currentHeath = 20;
-	static int totalHeath = 20;
-	static boolean inCave = false;
-	static int monsterHP = 15;
-	static int monsterLevel = 1;
-	static int numOfPotions =0;
-	static int numOfTurns = 0;
+	 int level = 1;
+	 int  exp;
+	 int expNeeded = 10 ;
+	 int currentHeath = 20;
+	 int totalHeath = 20;
+	 boolean inCave = false;
+	 int monsterHP = 10;
+	 int monsterLevel = 1;
+	 int numOfPotions =0;
+	 int numOfTurns = 0;
+
 	static String name;
 	static Scanner in = new Scanner(System.in);
 	 
 	public static void main(String[] args)
 	{
-		showMain();
-
+		new Adventure();
 	}
 	public Adventure()
 	{
-		//"What is your name, adventurer? : ";
-		//name= in.next();
+		System.out.println("What is your name, adventurer? : ");
+		name= in.next();
+		System.out.println("You awaken in a large field with no memory of how you got there. You can recal your name, but not much else.\n"
+				+ "You look around, but there is nothing to that you can see in the distance. Maybe there will be a town nearby. \n"
+				+ "You have a sword with you, but nothing else in your bag. What will you do? " );
+		showMain();
 	}
-	static void  showMain()
+	 void  showMain()
 	{
 		int choice;
 		System.out.println("1: Explore the Area\n2: Items\n3: Player Info\n4: Quit ");
-		System.out.print("Enter the Number of what you want to do ");
+		System.out.print("?");
 		choice = Integer.parseInt(in.next());
 		if(choice == 1 )
 		{
@@ -47,29 +51,35 @@ public class Adventure
 		}
 		else
 		{
+			//System.out.println("Are you sure you want to quit? Y/N");
+			//String ans = in.next();
+			//if(ans = "Y")
+			//{
 			quit();
+			//}
+			//showMain();
 		}
 	}
-	static void explore()
+	 void explore()
 	{
 		
-		int result = rollnum10();
+		int result = rollnum(0,10);
 		//did we find cave
-		if(result == 1 | result == 2)
+		if(result == 1 | result == 2 | result == 3)
 		{
 			System.out.println("You Found a Cave!");
 			inCave = true;
 			inCave();
 		}
 		//did we find healing center
-		else if(result == 3 | result == 4 | result == 5 )
+		else if(result == 4 | result == 5 )
 		{
 			System.out.println("You Found A Healing Center! Your Heath has been Restored");
 			currentHeath = totalHeath;
 		}
 		else
 		{
-			result = rollnum10();
+			result = rollnum(0, 5);
 			System.out.println("You Continue in the Field");
 			//did we find a potion
 			if(result == 1)
@@ -87,25 +97,25 @@ public class Adventure
 		showMain();
 		
 	}
-	static void inCave()
+	 void inCave()
 	{
-		
+		//monsters more frequent, but more potions/items
 	}
-	static void items()
+	 void items()
 	{
 		System.out.println("you have " + numOfPotions + " potions  \n Use a potion to restore 20 heath? Y/N ");
 		in.next();
 		System.out.println();
-		//display how many potions person has
+		//display how many potions/items person has
 	}
-	static void playerInfo()
+	 void playerInfo()
 	{
 		System.out.println("Level: " + level + "\n EXP Needed for Next Level: " + expNeeded 
 				+ "\n Heath: " + currentHeath  + "/" + totalHeath );
 		//exp and heath and level
 		
 	}
-	static void foundMonster()
+	 void foundMonster()
 	{
 			//monster fighting
 		int currentMonsterHP = monsterHP;
@@ -115,9 +125,9 @@ public class Adventure
 			System.out.println("1: Fight With Sword \n2: Items\n3: Player Info\n4: Run Away ");
 			System.out.print("Enter the Number of what you want to do ");
 			int choice = Integer.parseInt(in.next());
-			if(choice == 1 )
+			if(choice == 1 ) //you attacking
 			{
-				int damageDone= rollnum10();
+				int damageDone= rollnum(4,10) + level;
 				currentMonsterHP -= damageDone;
 ;				System.out.println("You Strike the monster with your sword " +  damageDone  + "Damage! "
 						+ "\n Monster heath: " + currentMonsterHP + "\\" + monsterHP );
@@ -130,17 +140,29 @@ public class Adventure
 			{
 				playerInfo();
 			}
+			else
+			{
+				 int chance = rollnum(1, 5);
+				 if(chance == 1 | chance == 2 | chance == 3)
+				 {
+					 System.out.println("got away safety");
+					 showMain();
+				 }
+				 else
+				 {
+					 System.out.println("you can't get away!");
+				 }
+			}
+			//monster attacks
 		}
+		showMain();
 	}
-	static int rollnum10()
+	
+	static int rollnum(int begin, int end)
 	{
-		 return (int) (Math.random() * ((10-1 + 1)) + 1);
+		 return (int) (Math.random() * ((begin-end + end)) + 1);
 	}
-	static int rollnum5()
-	{
-		 return (int) (Math.random() * ((5-1 + 1)) + 1);	
-	}
-	static boolean stillAlive()
+	 boolean stillAlive()
 	{
 		if(totalHeath >= currentHeath)
 		{
@@ -151,11 +173,18 @@ public class Adventure
 			return false;
 		}
 	}
-	static void levelUp()
+	 void levelUp()
 	{
-		//increase
+		 level++;
+		 totalHeath += 5;
+		 monsterLevel++;
+		 monsterHP+=2;
+		 expNeeded = exp * 2;
+		 exp = 0;
+		 currentHeath = totalHeath;
+		 System.out.println("You are now Level " + level + "!\nYour Heath has been Restored");
 	}
-	static void quit()
+	 void quit()
 	{
 		System.exit(0);
 	}
